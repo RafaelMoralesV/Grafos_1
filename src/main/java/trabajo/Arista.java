4b4c625bc6326344f6234872610952ed4a535d28
RafaelMoralesV <rmorales@utem.cl>
2020-10-15 21:56:06 -0300

    Nueva funcionalidad 'es conexo'

--- src/main/java/trabajo/Arista.java
+++ src/main/java/trabajo/Arista.java
@@ -1,38 +1,44 @@
 package trabajo;
 
 public class Arista {
-	
-	/* Clase de apoyo diseñada para funcionar con grafos con peso
+
+	/*
+	 * Clase de apoyo diseñada para funcionar con grafos con peso
 	 */
 	private float peso;
 	private Vertice inicio;
 	private Vertice destino;
-	
+
 	public float getPeso() {
 		return peso;
 	}
+
 	public void setPeso(float peso) {
 		this.peso = peso;
 	}
+
 	public Vertice getInicio() {
 		return inicio;
 	}
+
 	public void setInicio(Vertice inicio) {
 		this.inicio = inicio;
 	}
+
 	public Vertice getDestino() {
 		return destino;
 	}
+
 	public void setDestino(Vertice destino) {
 		this.destino = destino;
 	}
-	
-	public Arista(Vertice inicio, Vertice destino, float peso){
+
+	public Arista(Vertice inicio, Vertice destino, float peso) {
 		this.peso = peso;
 		this.inicio = inicio;
 		this.destino = destino;
 	}
-	
+
 	public Arista(String inicio, String destino, float peso) {
 		Vertice in = new Vertice(inicio);
 		Vertice out = new Vertice(destino);
@@ -40,8 +46,7 @@ public class Arista {
 		this.inicio = in;
 		this.destino = out;
 	}
-	
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -51,6 +56,7 @@ public class Arista {
 		result = prime * result + Float.floatToIntBits(peso);
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -63,15 +69,16 @@ public class Arista {
 		if (destino == null) {
 			if (other.destino != null)
 				return false;
-		} else if (!destino.equals(other.destino))
+		} else if (!destino.equals(other.destino)) {
 			return false;
+		}
 		if (inicio == null) {
-			if (other.inicio != null)
+			if (other.inicio != null) {
 				return false;
-		} else if (!inicio.equals(other.inicio))
+			}
+		} else if (!inicio.equals(other.inicio)) {
 			return false;
-		if (Float.floatToIntBits(peso) != Float.floatToIntBits(other.peso))
-			return false;
-		return true;
+		}
+		return Float.floatToIntBits(peso) != Float.floatToIntBits(other.peso);
 	}
 }
--- src/main/java/trabajo/Grafo.java
+++ src/main/java/trabajo/Grafo.java
@@ -3,115 +3,135 @@ package trabajo;
 import java.util.*;
 
 public class Grafo {
-	/* Clase principal que describe un grafo basico
-	 * Contiene metodos para funcionar como grafo sin peso, con o sin direcciones para las aristas
+	/*
+	 * Clase principal que describe un grafo basico Contiene metodos para funcionar
+	 * como grafo sin peso, con o sin direcciones para las aristas
 	 */
-	
+
 	private Map<Vertice, List<Vertice>> vertAdyacentes;
-	
+
 	public Grafo() {
 		this.vertAdyacentes = new HashMap<>();
 	}
-	
-	//								//
-	// 			VERTICES			//
-	//								//
+
+	// //
+	// VERTICES //
+	// //
 
 	// Agregar nuevo vertice
 	public void agregarVertice(String nombre) {
 		this.vertAdyacentes.putIfAbsent(new Vertice(nombre), new ArrayList<>());
 	}
-	
+
 	public void eliminarVertice(String nombre) {
 		Vertice v = new Vertice(nombre);
-		
+
 		// Elimina la lista de vertices adyacentes a 'nombre'
 		this.vertAdyacentes.values().stream().forEach(e -> e.remove(v));
-		
+
 		// Elimina el vertice del grafo
 		this.vertAdyacentes.remove(new Vertice(nombre));
 	}
-	
-	//								//
-	// 			ARISTAS				//
-	//								//
-	
+
+	// //
+	// ARISTAS //
+	// //
+
 	// Agrega una arista en una sola direccion
 	public void agregarAristaDir(String nombre1, String nombre2) {
 		Vertice v1 = new Vertice(nombre1);
 		Vertice v2 = new Vertice(nombre2);
 		this.vertAdyacentes.get(v1).add(v2);
 	}
-	
+
 	// Agrega una arista entre vertices
 	public void agregarArista(String nameA, String nameB) {
 		this.agregarAristaDir(nameA, nameB);
 		this.agregarAristaDir(nameB, nameA);
 	}
-	
+
 	// Elimina una sola arista entre <nombre1> y <nombre2>
 	public void eliminarAristaDir(String nombre1, String nombre2) {
 		Vertice v1 = new Vertice(nombre1);
-	    Vertice v2 = new Vertice(nombre2);
-	    List<Vertice> eV1 = this.vertAdyacentes.get(v1);
-	    if (eV1 != null)
-	        eV1.remove(v2);
+		Vertice v2 = new Vertice(nombre2);
+		List<Vertice> eV1 = this.vertAdyacentes.get(v1);
+		if (eV1 != null)
+			eV1.remove(v2);
 	}
-	
+
 	// Elimina todas las aristas entre <nombre1> y <nombre2>
 	public void eliminarAristas(String nameA, String nameB) {
-	    this.eliminarAristaDir(nameA, nameB);
-	    this.eliminarAristaDir(nameB, nameA);
-	} 
-	
-	//								//
-	// 			UTILS				//
-	//								//
-	
+		this.eliminarAristaDir(nameA, nameB);
+		this.eliminarAristaDir(nameB, nameA);
+	}
+
+	// //
+	// UTILS //
+	// //
+
 	public int sizeVertices() {
 		return this.vertAdyacentes.size();
 	}
-	
+
 	public int sizeAdyacentes(String nombre) {
 		Vertice v = new Vertice(nombre);
 		return this.vertAdyacentes.get(v).size();
 	}
-	
-	
-	public boolean[][] generarMatriz ()
-	{
+
+	public boolean[][] generarMatriz() {
 		// Dimensiones y nueva matriz de adyacencia
 		int numVerts = vertAdyacentes.size();
-		boolean[][] matrizAdy = new boolean [numVerts][numVerts];
-		
+		boolean[][] matrizAdy = new boolean[numVerts][numVerts];
+
 		// Generado nuevo arreglo con los vertices en el grafo
 		Set<Vertice> keys = vertAdyacentes.keySet();
 		Vertice[] arrVerts = keys.toArray(new Vertice[keys.size()]);
-		
+
 		// Se rellena la matriz segun la lista asociada a cada vertice
-		for(int i = 0; i<arrVerts.length; i++) {
-			for(int j=0; j<arrVerts.length; j++) {
+		for (int i = 0; i < arrVerts.length; i++) {
+			for (int j = 0; j < arrVerts.length; j++) {
 				matrizAdy[i][j] = vertAdyacentes.get(arrVerts[i]).contains(arrVerts[j]);
 			}
 		}
 		return matrizAdy;
 	}
-	
-	public String toString()
-	{
+
+	public String toString() {
 		StringBuilder s = new StringBuilder();
 		boolean[][] matrizAdy = this.generarMatriz();
-		
-	    for (int i = 0; i < vertAdyacentes.size(); i++) {
-	      s.append(i + ": ");
-	      for (boolean j : matrizAdy[i]) {
-	        s.append((j ? 1 : 0) + " ");
-	      }
-	      s.append("\n");
-	    }
-	    return s.toString();
-	   
-	  }
-	
+
+		for (int i = 0; i < vertAdyacentes.size(); i++) {
+			s.append(i + ": ");
+			for (boolean j : matrizAdy[i]) {
+				s.append((j ? 1 : 0) + " ");
+			}
+			s.append("\n");
+		}
+		return s.toString();
+
 	}
 	
+	
+	
+	@SuppressWarnings("unlikely-arg-type")
+	public boolean esConexo() {
+		boolean[][] matrizAdy = this.generarMatriz();		
+		
+		for(int i = 0; i < vertAdyacentes.size(); i++) {
+			// Checkea valores para columnas
+			boolean col = false;
+			for(int j = 0; j < vertAdyacentes.size(); j++) {
+				if(matrizAdy[i][j]) {
+					col = true;
+					break;
+				}
+			}
+			// Checkea valores para filas, y si solo existen 0 en ambas concluye que el grafo es inconexo
+			if(!Arrays.asList(matrizAdy[i]).contains(true) && col) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+}
